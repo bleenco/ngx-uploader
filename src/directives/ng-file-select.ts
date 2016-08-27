ff7e48c1300c02fd4f9594a19b3670f20360cf6a
Denko <denkomanceski@gmail.com>
2016-08-27 16:03:00 +0200

    added preview event

--- src/directives/ng-file-select.ts
+++ src/directives/ng-file-select.ts
@@ -4,14 +4,14 @@ import {Ng2Uploader} from '../services/ng2-uploader';
 @Directive({
   selector: '[ng-file-select]',
   inputs: ['options: ng-file-select'],
-  outputs: ['onUpload'],
+  outputs: ['onUpload', 'onPreviewData'],
   host: { '(change)': 'onFiles()' }
 })
 export class NgFileSelect {
   uploader: Ng2Uploader;
   options: any;
   onUpload: EventEmitter<any> = new EventEmitter();
-
+  onPreviewData: EventEmitter<any> = new EventEmitter();
   constructor(public el: ElementRef) {
     this.uploader = new Ng2Uploader();
     setTimeout(() => {
@@ -21,6 +21,9 @@ export class NgFileSelect {
     this.uploader._emitter.subscribe((data: any) => {
       this.onUpload.emit(data);
     });
+    this.uploader._previewEmitter.subscribe((data: any) => {
+      this.onPreviewData.emit(data);
+    })
   }
 
   onFiles(): void {
