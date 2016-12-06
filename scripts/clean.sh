c69c76527fb8302f290dd66f6b70fa37bacc56c3
Celso Santos <celso.santos@syone.com>
2016-12-06 10:56:41 +0000

    Commit unsaved files

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -2,5 +2,5 @@
 
 find . -name "*.js" -type f -not -path "./node_modules/*" -delete
 find . -name "*.js.map" -type f -not -path "./node_modules/*" -delete
-find . -name "*.d.ts" -type f -not -path "./node_modules/*" -
+find . -name "*.d.ts" -type f -not -path "./node_modules/*" -delete
 find . -name "*.metadata.json" -type f -not -path "./node_modules/*" -delete
