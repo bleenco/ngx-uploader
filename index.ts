8ba6cc161221fd49dd94ce6af630a2048830fbe8
Jan Kuri <jkuri88@gmail.com>
2016-12-09 18:21:53 +0100

    fix: 1.6.1

---
+++ index.ts
@@ -0,0 +1,5 @@
+export * from './src/directives/ng-file-drop';
+export * from './src/directives/ng-file-select';
+export * from './src/services/ng2-uploader';
+
+export { Ng2UploaderModule } from './src/module/ng2-uploader.module';
