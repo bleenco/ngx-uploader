e1e1285fad9a6dde11025201a938ea67775b0d4f
Jan Kuri <jkuri88@gmail.com>
2017-02-16 20:38:41 +0100

    build(umd): umd bundles

--- ngx-uploader.ts
+++ ngx-uploader.ts
@@ -1,1 +1,15 @@
-export * from './index';
+import { NgFileDropDirective } from './src/directives/ng-file-drop';
+import { NgFileSelectDirective } from './src/directives/ng-file-select';
+
+export * from './src/directives/ng-file-drop';
+export * from './src/directives/ng-file-select';
+export * from './src/services/ngx-uploader';
+
+export { NgUploaderOptions, UploadedFile, UploadRejected } from './src/classes/index';
+
+export { NgUploaderModule } from './src/module/ngx-uploader.module';
+
+export const UPLOAD_DIRECTIVES: any[] = [
+  NgFileSelectDirective,
+  NgFileDropDirective
+];
