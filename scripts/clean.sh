b60c161726a6d02965e999747a471487fd18d1b7
Jan Kuri <jkuri88@gmail.com>
2017-02-16 21:35:47 +0100

    chore: update deps

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-find . -name "*.js" -type f -and -not -path "./node_modules/*" -and -not -path "./gulpfile.js" -and -not -path "./make.js" -delete
+find . -name "*.js" -type f -and -not -path "./node_modules/*" -and -not -path "./gulpfile.js" -and -not -path "./rollup.config.js" -delete
 find . -name "*.js.map" -type f -not -path "./node_modules/*" -delete
 find . -name "*.d.ts" -type f -not -path "./node_modules/*" -delete
 find . -name "*.metadata.json" -type f -not -path "./node_modules/*" -delete
