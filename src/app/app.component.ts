f33b90a64218bc26d7e0e11dcc98521ae8e61655
Sergio <sergiobop93@gmail.com>
2018-06-18 13:30:02 +0200

    Added maxFiles feature

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,7 +14,7 @@ export class AppComponent {
   options: UploaderOptions;
 
   constructor() {
-    this.options = { concurrency: 1 };
+    this.options = { concurrency: 1, maxUploads: 3 };
     this.files = [];
     this.uploadInput = new EventEmitter<UploadInput>();
     this.humanizeBytes = humanizeBytes;
