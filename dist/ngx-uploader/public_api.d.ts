0907e709248f5f701266bce4e911296bc54f031c
Sujendra shrestha <sujendra.shrestha@itonics.de>
2018-12-12 05:15:40 +0545

    package update

--- dist/ngx-uploader/public_api.d.ts
+++
@@ -1,5 +0,0 @@
-export * from './lib/interfaces';
-export * from './lib/ng-file-drop.directive';
-export * from './lib/ng-file-select.directive';
-export * from './lib/ngx-uploader.class';
-export * from './lib/ngx-uploader.module';
